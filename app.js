fab7c05e097ac4fa54301c387af86c45a79ff04e
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-12-24 16:29:24 +0000

    created campgrounds page, parsing fake data with 3 campgrounds

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get('/', function( req, res){
     res.render('landing')
 })
 
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name: "The Roaches", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
+        {name: "The Cloud", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"},
+        {name: "Macclesfield Forest", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"}
+    ]
+    res.render("campgrounds", {campgrounds: campgrounds})
+})
+
 app.listen(3000, function(){
     console.log('listening on localhost 3000')
 })
