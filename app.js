3c7a35d0f57be0386cfa81ab5f692e8869924452
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-12-26 19:50:58 +0000

    /campgrounds/new has a form that takes in a text input and url input. Redirects to /campgrounds once posted. Data currently being pushed into campgrounds array in app.js

--- app.js
+++ app.js
@@ -5,21 +5,31 @@ var bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 
+var campgrounds = [
+    {name: "The Roaches", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
+    {name: "The Cloud", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"},
+    {name: "Macclesfield Forest", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"}
+]
+
 app.get('/', function( req, res){
     res.render('landing')
 })
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "The Roaches", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
-        {name: "The Cloud", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"},
-        {name: "Macclesfield Forest", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"}
-    ]
     res.render("campgrounds", {campgrounds: campgrounds})
 })
 
 app.post("/campgrounds", function(req, res){
-    res.send("you hit the post route")
+    //get data from the form and add to campgrounds array
+    var name = req.body.name
+    var image = req.body.image
+    var newCampground = {
+        name: name,
+        image: image
+    }
+    campgrounds.push(newCampground)
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds")
 })
 
 app.get("/campgrounds/new", function(req, res){
