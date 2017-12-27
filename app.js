234459acbb0701084ed3f3bd613967ef8b68449b
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-12-27 09:43:11 +0000

    bootstrap styling added to navbar on the /campgrounds page

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs")
 var campgrounds = [
     {name: "The Roaches", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
     {name: "The Cloud", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"},
+    {name: "Macclesfield Forest", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"},
+    {name: "The Roaches", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg"},
+    {name: "The Cloud", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"},
     {name: "Macclesfield Forest", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"}
 ]
 
