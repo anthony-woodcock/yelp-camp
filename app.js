db550b575247d17845e6103eebe3abbe3c66d548
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-12-28 13:45:32 +0000

    Started creating a new get request to campgrounds/:id which will display indivdual campgrounds with more infornamtion. Description has been added to the schema

--- app.js
+++ app.js
@@ -10,28 +10,31 @@ app.set("view engine", "ejs")
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 })
 
 var Campground = mongoose.model("Campground", campgroundSchema)
 
-// Campground.create({
-//     name: "The Cloud",
-//     image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg",
-// }, function(err, campground){
-//     if(err){
-//         console.log(err)
-//     }else {
-//         console.log("Newly created campground")
-//         console.log(campground)
-//     }
-// })
+Campground.create({
+    name: "The Cloud",
+    image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg",
+    description: "This is a huge granite hill, no bathrooms. No water. Beautful granite"
+}, function(err, campground){
+    if(err){
+        console.log(err)
+    }else {
+        console.log("Newly created campground")
+        console.log(campground)
+    }
+})
 
 
 app.get('/', function( req, res){
     res.render('landing')
 })
 
+//Index route
 app.get("/campgrounds", function(req, res){
     // Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
@@ -43,6 +46,7 @@ app.get("/campgrounds", function(req, res){
     })
 })
 
+//CREATE add new campground to daatabase
 app.post("/campgrounds", function(req, res){
     //get data from the form and add to campgrounds array
     var name = req.body.name
@@ -61,8 +65,15 @@ app.post("/campgrounds", function(req, res){
     })
 })
 
+//NEW - show for to create new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs")
+})
+
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    //render show template with that campground
+    res.send("THIS WILL BE THE SHOW PAGE ONE DAY")
 })
 
 app.listen(3000, function(){
