36d6770678b41631b6099f5b67fb662cd6c44223
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2018-01-03 21:53:59 +0000

    new directory public with stylsheet. Bottstrap styling added as well

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Cloud's Rest",
         image: "https://www.decathlon.co.uk/ecuk/static/wedze/assets/img/camping/camping-background.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dignissim, nulla a auctor semper, leo diam tincidunt diam, non sagittis magna velit ultrices felis. Etiam sed orci ut leo pharetra feugiat nec eget diam. "
     },
     {
         name: "Desert Sand",
         image: "https://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/06/main/fall-camping-best-campgrounds-organ-pipe-cactus-national-monument-twin-peaks-1115.jpg?itok=cQMlidOg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dignissim, nulla a auctor semper, leo diam tincidunt diam, non sagittis magna velit ultrices felis. Etiam sed orci ut leo pharetra feugiat nec eget diam. Sed nec vulputate mi. Aliquam erat turpis, convallis et elementum "
     },
     {
         name: "Forest Hill",
         image: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?dpr=1&auto=format&fit=crop&w=376&h=251&q=60&cs=tinysrgb",
-        description: "blah blah blah"
+        description: "us in tellus. In eget metus eget est congue tempus. Phasellus sed commodo nulla. Proin nulla urna, scelerisque semper lobortis et, accumsan sagittis dui. Vestibulum convallis nisi non consectetur mattis. Etiam in consequat urna. Praesent quis dolor diam. Vivamus ultrices sapien leo, et trist"
     }
 ]
 
