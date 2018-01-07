952f02ea573ddebee347550f899c9bf40148d410
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2018-01-07 16:46:02 +0000

    login route created

--- app.js
+++ app.js
@@ -144,6 +144,11 @@ app.post("/register", function(req, res){
     })
 })
 
+// show login form
+app.get('/login', function(req, res){
+    res.render("login")
+})
+
 app.listen(3000, function(){
     console.log('listening on localhost 3000')
 })
