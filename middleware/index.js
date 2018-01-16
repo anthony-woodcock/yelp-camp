79c93a7dda052652f916941602179f5f5a803ea4
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2018-01-16 20:09:45 +0000

    flash route set up if user cant find campground

--- middleware/index.js
+++ middleware/index.js
@@ -7,12 +7,14 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
     if(req.isAuthenticated()){
         Campground.findById(req.params.id, function(err, foundCampground){
             if(err){
+                req.flash("error", "Campground not found")
                 res.redirect("back")
             } else {
                 //does user own the campground
                 if(foundCampground.author.id.equals(req.user._id)){
                     next()
                 } else {
+                    req.flash("error", "You don't have permission to do that")
                     res.redirect("back")
                 }
             }
